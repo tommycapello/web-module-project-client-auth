4615d0a36e52a56f013cc43fd35ef27fee2820a2
Thomas Capello <thomcapello@gmail.com>
2021-04-23 17:19:36 -0400

    corrected project

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/AddFriendForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello Friends!</h1>
+        <Link to='/friends' style={{ marginRight: "16px"}}>See Friend</Link>
+        <Link to='/friends/add-new'>Add Friend</Link>
+        <Switch>
+          <PrivateRoute path='/friends/add-new' component={AddFriendForm}/>
+          <PrivateRoute path='/friends' component={FriendsList}/>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
